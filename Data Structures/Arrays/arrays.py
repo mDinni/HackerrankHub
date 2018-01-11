9d27ae16f7e4a4ff708046fb9b80e74e5d3e9cb1
Dinesh Singh <dineshsingh9375@gmail.com>
2018-01-11 10:50:24 +0530

    Print all 'n' integers in 'arr' in reverse order as a single line of space-separated integers

--- Data Structures/Arrays/arrays.py
+++ Data Structures/Arrays/arrays.py
@@ -1,7 +1,18 @@
 #!/bin/python3
 
+# Take the input, trim the whitespace using strip() and convert the string into integer
+# eg: > input() --> '  5 '
+#     > strip() --> '5'
+#     > int()   -->  5
 n = int(input().strip())
-arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+# Use list comprehension, split(default=' ') gets a list based on the delimiter specified
+# and then converting, each string element in the list to integer
+arr = [int(arr_temp) for arr_temp in input().strip().split()]
+
+# Printing elements in Reverse Order
+# Can also be done by
+# print(arr[::-1])
 
 while n:
     n -= 1
